a5dfce611070708a8a3cd9bbed111092a27ff171
Darshanproject <gadadarshan6184@gmail.com>
2023-07-31 12:57:04 +0530

    Completed

---
+++ Backuplectures/first.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+		int  even;
+		printf("Enter : ");
+		scanf("%d",&even);
+		printf("%d",even);
+}
+
+/*
+user 10 
+even +
+oddd +
+even count 
+odd count 
+*/
---
+++ Backuplectures/h1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int num,num1,ans;
+	printf("Enter your number here : ");
+	scanf("%d",&num);
+	printf("Enter your second number : ");
+	sacnf("%d",&num1);
+	ans=num+num1;
+	printf("%d",ans);
+}
